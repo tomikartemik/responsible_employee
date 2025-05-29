e63ed7ab680017091a614ac7c1a0653da138c37f
tomikartemik <artemules@mail.ru>
2025-05-29 03:07:19 +0300

    UPD: IDs

--- internal/model/risk_report.go
+++ internal/model/risk_report.go
@@ -1,7 +1,7 @@
 package model
 
 type RiskReport struct {
-	ID          string `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
+	ID          string `gorm:"primaryKey;type:uuid;default:gen_random_uuid()" json:"id"`
 	Category    string `gorm:"not null" json:"category"`
 	Type        string `gorm:"not null" json:"type"`
 	Description string `gorm:"not null" json:"description"`
