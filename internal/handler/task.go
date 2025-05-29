f1afd0bd5af2971257e2e869754e1b8366b1a763
tomikartemik <artemules@mail.ru>
2025-05-29 03:34:03 +0300

    UPD: handlers

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -29,22 +29,25 @@ func (h *Handler) GetAllTasks(c *gin.Context) {
 
 	if err != nil {
 		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	c.JSON(http.StatusOK, tasks)
 }
 
 func (h *Handler) GetTaskById(c *gin.Context) {
-	id := c.Param("id")
+	id := c.Query("id")
 
 	if id == "" {
 		utils.NewErrorResponse(c, http.StatusBadRequest, errors.New("Need task ID!").Error())
+		return
 	}
 
 	task, err := h.services.TaskByID(id)
 
 	if err != nil {
 		utils.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	c.JSON(http.StatusOK, task)
