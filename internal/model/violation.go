4f6b9d0c7621409cfa828122dd8de39380f233a3
tomikartemik <artemules@mail.ru>
2025-06-03 06:17:09 +0300

    violation added to task

--- internal/model/violation.go
+++ internal/model/violation.go
@@ -1,7 +1,7 @@
 package model
 
 type Violation struct {
-	ID          int    `gorm:"primaryKey;autoIncrement" json:"ID"`
+	ID          int    `gorm:"primaryKey;uniqueIndex;autoIncrement" json:"id"`
 	Category    string `gorm:"not null" json:"category"`
 	Description string `gorm:"not null" json:"description"`
 	RiskLevel   string `gorm:"not null" json:"risk_level"`
