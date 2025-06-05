000a11f1608b6bb9b9ef66afd9d4035bdb414cb5
tomikartemik <artemules@mail.ru>
2025-06-06 02:53:01 +0300

    violation added to task

--- internal/repository/task.go
+++ internal/repository/task.go
@@ -21,7 +21,7 @@ func (r *TaskRepository) CreateTask(task model.Task) error {
 func (r *TaskRepository) GetAllTasks() ([]model.Task, error) {
 	var tasks []model.Task
 
-	err := r.db.Find(&tasks).Where("status = Active").Preload("Violation").Error
+	err := r.db.Find(&tasks).Where("status = Active").Preload("violations").Error
 	if err != nil {
 		return []model.Task{}, err
 	}
@@ -32,7 +32,7 @@ func (r *TaskRepository) GetAllTasks() ([]model.Task, error) {
 func (r *TaskRepository) TaskByID(taskID string) (model.Task, error) {
 	var task model.Task
 
-	err := r.db.Where("id = ?", taskID).Preload("Violation").First(&task).Error
+	err := r.db.Where("id = ?", taskID).Preload("violations").First(&task).Error
 	if err != nil {
 		return model.Task{}, err
 	}
